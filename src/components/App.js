564bffe998cc1db63d21ffa7daf3d7105b51aa13
Brian Bycynski <high5bri@hotmail.com>
2018-03-09 12:33:29 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import classes from "../css/app.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className={classes.App_h1}>BDOG</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
